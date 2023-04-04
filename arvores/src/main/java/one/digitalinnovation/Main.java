328278b8782a4d6cf4ee9659580c8799d4a86bd2
ErickAraujodC <erickbyborges@gmail.com>
2023-04-03 22:41:51 -0300

    arvore bin done

--- arvores/src/main/java/one/digitalinnovation/Main.java
+++ arvores/src/main/java/one/digitalinnovation/Main.java
@@ -1,7 +1,22 @@
 package one.digitalinnovation;
 
+import one.digitalinnovation.model.Obj;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArvoreBinaria<Obj> minhaArvore = new ArvoreBinaria<>(){{
+            inserir(new Obj(13));
+            inserir(new Obj(10));
+            inserir(new Obj(25));
+            inserir(new Obj(2));
+            inserir(new Obj(12));
+            inserir(new Obj(20));
+            inserir(new Obj(31));
+            inserir(new Obj(29));
+        }};
+
+        minhaArvore.exibirInOrdem();
+        minhaArvore.exibirPosOrdem();
+        minhaArvore.exibirPreOrdem();
     }
 }
